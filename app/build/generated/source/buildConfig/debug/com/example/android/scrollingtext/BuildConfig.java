787d11135478e69443b2e0800d98683e6a98c3de
mancy1998 <deepmansi2017@gmail.com>
2021-03-20 22:30:07 +0545

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/android/scrollingtext/BuildConfig.java
@@ -0,0 +1,11 @@
+
+package com.example.android.scrollingtext;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.android.scrollingtext";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
